437c04dba126e6a76959c08e729b1875a93f224b
Jv2350 <tp8375735@gmail.com>
2025-06-14 00:24:25 +0530

    demonstrate why we need useState and remove unnecessary files

--- 5_state-management-basics/src/App.js
+++ 5_state-management-basics/src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  let count = 0;
+
+  function handleAdd(){
+    count+=1;
+    console.log(count);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="box">
+        <p>{count}</p>
+        <button onClick={handleAdd} className='add'>ADD</button>
+        <button className='sub'>SUB</button>
+      </div>
     </div>
   );
 }

25ce5b114b5639bffc897f8c373046d10815a2bc
Jv2350 <tp8375735@gmail.com>
2025-06-15 23:12:06 +0530

    introduce useState hook for basic state management

--- 5_state-management-basics/src/App.js
+++ 5_state-management-basics/src/App.js
@@ -1,19 +1,34 @@
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
 function App() {
-  let count = 0;
+  const [count, setCount] = useState(0);
 
-  function handleAdd(){
-    count+=1;
-    console.log(count);
+  function handleAdd() {
+    setCount(count + 1);
+  }
+
+  function handleSub() {
+    setCount(count - 1);
+  }
+
+  function handleReset() {
+    setCount(0);
   }
 
   return (
     <div className="App">
       <div className="box">
         <p>{count}</p>
-        <button onClick={handleAdd} className='add'>ADD</button>
-        <button className='sub'>SUB</button>
+        <button onClick={handleAdd} className="add">
+          ADD
+        </button>
+        <button onClick={handleSub} className="sub">
+          SUB
+        </button>
+        <button onClick={handleReset} className="reset">
+          RESET
+        </button>
       </div>
     </div>
   );

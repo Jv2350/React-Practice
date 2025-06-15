d49852335d90c2ffa78ef1157e051e60748bd4ac
Jv2350 <tp8375735@gmail.com>
2025-06-15 23:15:07 +0530

    implement multiple state updates with useState

--- 5_state-management-basics/src/App.js
+++ 5_state-management-basics/src/App.js
@@ -5,7 +5,9 @@ function App() {
   const [count, setCount] = useState(0);
 
   function handleAdd() {
-    setCount(count + 1);
+    setCount((count) => count + 1);
+    setCount((count) => count + 1);
+    setCount((count) => count + 1);
   }
 
   function handleSub() {
